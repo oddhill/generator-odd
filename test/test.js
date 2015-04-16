07a7ec9902c8018582b87651605a7134249ddbc2
Jens Lind <jens.lind@oddhill.se>
2015-04-16 12:58:30 +0200

    Added test cases for oddbaby generator

---
+++ test/test.js
@@ -0,0 +1,27 @@
+describe('generator-odd', function () {
+
+  describe('When generating Oddbaby', function () {
+
+    before(function () {
+
+    });
+
+    it('Should clone Oddbaby and remove the git repository', function () {
+
+    });
+
+    it('Should rename oddbaby.info', function () {
+
+    });
+
+    it('Should replace the word "oddbaby" in various files', function() {
+
+    });
+
+    it('Should run "npm install"', function () {
+
+    });
+
+  });
+
+});
