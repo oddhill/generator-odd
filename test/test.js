30081fd8238cd76bf11c30f52a6bb521672fad17
Jens Lind <jens.lind@oddhill.se>
2015-04-16 19:40:52 +0200

    Added odd:baby tests

--- test/test.js
+++ test/test.js
@@ -1,25 +1,42 @@
+var helpers = require('yeoman-generator').test;
+var fse = require('fs-extra');
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+
 describe('generator-odd', function () {
 
   describe('When generating Oddbaby', function () {
 
-    before(function () {
-
+    before(function (done) {
+      // Run the generator
+      helpers.run(path.join(__dirname, '../generators/baby'))
+        .inDir(path.join(__dirname, './tmp'))
+        .on('end', function () {
+          done();
+        });
     });
 
-    it('Should clone Oddbaby and remove the git repository', function () {
+    it('Should clone Oddbaby', function () {
+      assert.fileContent(__dirname + '/tmp/package.json', '"name": "oddbaby"');
+    });
 
+    it('Remove the git repository', function () {
+      assert.noFile(__dirname + '/tmp/.git');
     });
 
     it('Should rename oddbaby.info', function () {
-
+      assert.file(path.join(__dirname, './tmp/tmp.info'));
     });
 
-    it('Should replace the word "oddbaby" in various files', function() {
-
+    it('Should replace the word "oddbaby" in various files', function () {
+      var files = ['template.php', 'preprocess.inc', 'theme.inc', 'js/main.js'];
+      for (file in files) {
+        assert.noFileContent(path.join(__dirname, './tmp/' + files[file]), 'oddbaby');
+      }
     });
 
-    it('Should run "npm install"', function () {
-
+    after(function () {
+      process.exit();
     });
 
   });
