217e7738bd18581df7f93028a72f3ab3d49597e7
Jens Lind <jens.lind@oddhill.se>
2015-04-20 21:16:53 +0200

    Fixed path

--- test/test.js
+++ test/test.js
@@ -57,7 +57,7 @@ describe('generator-odd', function () {
     });
 
     it('Clone Odddrupal', function () {
-      assert.file(path.join(__dirname, '/tmp/drupal/index.php'));
+      assert.file(path.join(__dirname, '/tmp/index.php'));
     });
 
     it('Should remove remote reference', function () {
