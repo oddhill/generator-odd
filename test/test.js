c785e986355dfb9505ede0f32134575a06f2d7ad
Jens Lind <jens.lind@oddhill.se>
2015-08-08 16:39:22 +0200

    Updated tests

--- test/test.js
+++ test/test.js
@@ -12,6 +12,7 @@ describe('generator-odd', function () {
       this.timeout(10000)
       // Run the generator
       helpers.run(path.join(__dirname, '../generators/baby'))
+        .withPrompts({name: 'test'})
         .inDir(path.join(__dirname, './tmp'))
         .on('end', function () {
           done()
@@ -19,25 +20,25 @@ describe('generator-odd', function () {
     })
 
     it('Should clone Oddbaby', function () {
-      assert.fileContent(__dirname + '/tmp/package.json', '"name": "oddbaby"')
+      assert.fileContent(__dirname + '/tmp/test/package.json', '"name": "oddbaby"')
     })
 
     it('Remove the git repository', function () {
-      assert.noFile(__dirname + '/tmp/.git')
+      assert.noFile(__dirname + '/tmp/test/.git')
     })
 
     it('Should rename oddbaby.info', function () {
-      assert.file(path.join(__dirname, './tmp/tmp.info'))
+      assert.file(path.join(__dirname, './tmp/test/test.info'))
     })
 
     it('Should rename "Odd Baby" in .info file', function () {
-      assert.noFileContent(path.join(__dirname, './tmp/tmp.info'), 'Odd Baby')
+      assert.noFileContent(path.join(__dirname, './tmp/test/test.info'), 'Odd Baby')
     })
 
     it('Should replace the word "oddbaby" in various files', function () {
       var files = ['template.php', 'preprocess.inc', 'theme.inc', 'js/main.js']
       for (file in files) {
-        assert.noFileContent(path.join(__dirname, './tmp/' + files[file]), 'oddbaby')
+        assert.noFileContent(path.join(__dirname, './tmp/test/' + files[file]), 'oddbaby')
       }
     })
 
@@ -55,6 +56,7 @@ describe('generator-odd', function () {
       this.timeout(0)
       // Run the generator
       helpers.run(path.join(__dirname, '../generators/drupal'))
+        .withPrompts({name: 'test'})
         .inDir(path.join(__dirname, './tmp'))
         .on('end', function () {
           done()
@@ -62,36 +64,36 @@ describe('generator-odd', function () {
     })
 
     it('Clone Odddrupal', function () {
-      assert.file(path.join(__dirname, '/tmp/index.php'))
+      assert.file(path.join(__dirname, '/tmp/test/index.php'))
     })
 
     it('Should remove remote reference', function () {
-      exec('git remote', function (err, stdout, stderr) {
-        assert.textEqual('', '')
+      exec('git remote', {cwd: path.join(__dirname, '/tmp/test')}, function (err, stdout, stderr) {
+        assert.textEqual(stdout, '\n')
       })
     })
 
     it('Rename 7.x to master', function () {
-      exec('git rev-parse --abbrev-ref HEAD', function (err, stdout, stderr) {
-        assert.textEqual('master', 'master')
+      exec('git rev-parse --abbrev-ref HEAD', {cwd: path.join(__dirname, '/tmp/test')}, function (err, stdout, stderr) {
+        assert.textEqual(stdout, 'master\n')
       })
     })
 
     it('Should create .htaccess file', function () {
-      assert.file(path.join(__dirname, '/tmp/.htaccess'))
+      assert.file(path.join(__dirname, '/tmp/test/.htaccess'))
     })
 
     it('Should create settings.local.php file', function () {
-      assert.file(path.join(__dirname, '/tmp/sites/default/settings.local.php'))
+      assert.file(path.join(__dirname, '/tmp/test/sites/default/settings.local.php'))
     })
 
     it('Ensure files dir', function () {
-      assert.file(path.join(__dirname, '/tmp/sites/all/files'))
+      assert.file(path.join(__dirname, '/tmp/test/sites/all/files'))
     })
 
     it('Should change permission on files dir', function (done) {
       // Make sure files folder is 777
-      fse.stat(path.join(__dirname, '/tmp/sites/all/files'), function (err, stats) {
+      fse.stat(path.join(__dirname, '/tmp/test/sites/all/files'), function (err, stats) {
         assert.textEqual('40777', parseInt(stats.mode.toString(8), 10).toString())
         done()
       })
